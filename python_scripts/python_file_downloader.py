ea9bda6b2c9eac2418cb813b9211a233e98c4467
pgraffigna <pabloggraffigna@gmail.com>
2021-08-16 14:10:59 -0400

    Actualizaciòn de notas

--- python_scripts/python_file_downloader.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-# file downloader
-
-import requests
-
-url = 'https://assets.tryhackme.com/img/THMlogo.png'
-r = requests.get(url, allow_redirects=True)
-open('THMlogo.png', 'wb').write(r.content)
